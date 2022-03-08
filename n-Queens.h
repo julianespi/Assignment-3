917036ac8d97d165225fb2727ba077fa838030da
Julian Espinoza <99230462+julianespi@users.noreply.github.com>
2022-03-07 18:44:15 -0800

    Update n-Queens.h

--- n-Queens.h
+++ n-Queens.h
@@ -1,1 +1,21 @@
 #pragma once
+	int sizeOfBoard = inputInteger("Enter the size of the bored you want to play on: ", 1, 10);
+	for (int c = 0; c < sizeOfBoard; c++)
+	{
+		for (int i = 0; i < sizeOfBoard; i++)
+		{
+			if (i == 0)
+				cout << "|";
+			cout << " |";
+			if (i == sizeOfBoard - 1)
+				cout << endl;
+		}
+		for (int i = 0; i < sizeOfBoard; i++)
+		{
+			if (i == 0)
+				cout << "-";
+			cout << "--";
+			if (i == sizeOfBoard - 1)
+				cout << endl;
+		}
+	}
