c7ce9784a79d2de5abcc40065d63f688d2c7372e
Julian Espinoza <99230462+julianespi@users.noreply.github.com>
2022-03-09 14:30:47 -0800

    Update TTT_Wesley.cpp

--- TTT_Wesley.cpp
+++ TTT_Wesley.cpp
@@ -1,12 +1,6 @@
-#include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <Windows.h>
-#include <iomanip>
 #include "TTT_Wesley.h"
-
 using namespace std;
+
 //PreCondition: startRange (integer) and endRange (integer)
 //PostCondition: returns an integer value within range (startRannge and endRange)
 int TTT_W::inputInteger(string prompt, int startRange, int endRange)
@@ -30,7 +24,6 @@ int TTT_W::inputInteger(string prompt, int startRange, int endRange)
     cin.ignore(999, '\n');
     return input;
 }
-
 //precondition: user input for ft
 //postcondition: firstTurn is set
 void TTT_W::setFTurn(char ft)
@@ -47,7 +40,7 @@ void TTT_W::GameTTT()
 {
     //precondition: firstTurn is set;
     //postcondition: determines starting player
-    if (firstTurn == 'n')
+    if (firstTurn == 'N')
     {
         cout << "Computer will take the first move." << endl;
         Sleep(500); 	// delay for 500 milliseconds
@@ -61,7 +54,7 @@ void TTT_W::GameTTT()
     }
     //precondition: firstTurn is set
     //postcondition: determines starting player
-    if (firstTurn == 'y') 
+    if (firstTurn == 'Y')
     {
         cout << "Player will take the first move." << endl;
         Sleep(500); 	// delay for 500 milliseconds
@@ -75,7 +68,7 @@ void TTT_W::GameTTT()
     }
     //precondition: firstTurn is set
     //postcondition: input validation
-    if ((firstTurn != 'n') && (firstTurn != 'y')) {
+    if ((firstTurn != 'N') && (firstTurn != 'Y')) {
         cout << "Player has not given a valid answer";
     }
     //precondition: firstTurn = 'y' || 'n', winCondition == false
@@ -290,148 +283,147 @@ void TTT_W::GameTTT()
         }
         //precondition: turnCurrent = 1, computer has gone
         //postcondition: player's turn
-        if ((turnCurrent == 1) && (playerNow == true)) 
+        if ((turnCurrent == 1) && (playerNow == true))
         {
             cout << "Player's Turn." << endl;
             cout << "Board will be generated upon first move selection." << endl;
             //precondition: player's turn to input square coord
             //postcondition: valid coordinates set
-            cout << "Player select a number <1-9> to place your symbol in a square: " << endl;
-            cin >> turnSquare;
+            //cout << "Player select a number <1-9> to place your symbol in a square: " << endl;
+            turnSquare=inputInteger("Player select a number <1-9> to place your symbol in a square: ", 1,9);
             cout << endl;
             //precondition: turnSquare = 1
             //postcondition: coordinates on board are set
-            if (turnSquare == 1) 
+            if (turnSquare == 1)
             {
                 x = 0;
                 y = 0;
             }
             //precondition: turnSquare = 2
             //postcondition: coordinates on board are set
-            if (turnSquare == 2) 
+            if (turnSquare == 2)
             {
                 x = 0;
                 y = 1;
             }
             //precondition: turnSquare = 3
             //postcondition: coordinates on board are set
-            if (turnSquare == 3) 
+            if (turnSquare == 3)
             {
                 x = 0;
                 y = 2;
             }
             //precondition: turnSquare = 4
             //postcondition: coordinates on board are set
-            if (turnSquare == 4) 
+            if (turnSquare == 4)
             {
                 x = 1;
                 y = 0;
             }
             //precondition: turnSquare = 5
             //postcondition: coordinates on board are set
-            if (turnSquare == 5) 
+            if (turnSquare == 5)
             {
                 x = 1;
                 y = 1;
             }
             //precondition: turnSquare = 6
             //postcondition: coordinates on board are set
-            if (turnSquare == 6) 
+            if (turnSquare == 6)
             {
                 x = 1;
                 y = 2;
             }
             //precondition: turnSquare = 7
             //postcondition: coordinates on board are set
-            if (turnSquare == 7) 
+            if (turnSquare == 7)
             {
                 x = 2;
                 y = 0;
             }
             //precondition: turnSquare = 8
             //postcondition: coordinates on board are set
-            if (turnSquare == 8) 
+            if (turnSquare == 8)
             {
                 x = 2;
                 y = 1;
             }
             //precondition: turnSquare = 9
             //postcondition: coordinates on board are set
-            if (turnSquare == 9) 
+            if (turnSquare == 9)
             {
                 x = 2;
                 y = 2;
             }
             //precondition: selected square in board is filled
             //postcondition: reassigns selected square by player
-            while ((((turnSquare <= 0) || (turnSquare >= 10)) || (ticBoard[x][y] >= 1))) 
+            while ((((turnSquare <= 0) || (turnSquare >= 10)) || (ticBoard[x][y] >= 1)))
             {
                 cout << "Invalid Selection, give a valid number that has not been taken: ";
                 x = 3;
                 y = 3;
                 //precondition: player's turn to input square coord
                 //postcondition: valid coordinates set
-                cout << "Player select a number <1-9> to place your symbol in a square: " << endl;
-                cin >> turnSquare;
+                turnSquare = inputInteger("Player select a number <1-9> to place your symbol in a square: ", 1, 9);
                 //precondition: turnSquare = 1
                 //postcondition: coordinates on board are set
-                if (turnSquare == 1) 
+                if (turnSquare == 1)
                 {
                     x = 0;
                     y = 0;
                 }
                 //precondition: turnSquare = 2
                 //postcondition: coordinates on board are set
-                if (turnSquare == 2) 
+                if (turnSquare == 2)
                 {
                     x = 0;
                     y = 1;
                 }
                 //precondition: turnSquare = 3
                 //postcondition: coordinates on board are set
-                if (turnSquare == 3) 
+                if (turnSquare == 3)
                 {
                     x = 0;
                     y = 2;
                 }
                 //precondition: turnSquare = 4
                 //postcondition: coordinates on board are set
-                if (turnSquare == 4) 
+                if (turnSquare == 4)
                 {
                     x = 1;
                     y = 0;
                 }
                 //precondition: turnSquare = 5
                 //postcondition: coordinates on board are set
-                if (turnSquare == 5) 
+                if (turnSquare == 5)
                 {
                     x = 1;
                     y = 1;
                 }
                 //precondition: turnSquare = 6
                 //postcondition: coordinates on board are set
-                if (turnSquare == 6) 
+                if (turnSquare == 6)
                 {
                     x = 1;
                     y = 2;
                 }
                 //precondition: turnSquare = 7
                 //postcondition: coordinates on board are set
-                if (turnSquare == 7) 
+                if (turnSquare == 7)
                 {
                     x = 2;
                     y = 0;
                 }
                 //precondition: turnSquare = 8
                 //postcondition: coordinates on board are set
-                if (turnSquare == 8) 
+                if (turnSquare == 8)
                 {
                     x = 2;
                     y = 1;
                 }
                 //precondition: turnSquare = 9
                 //postcondition: coordinates on board are set
-                if (turnSquare == 9) 
+                if (turnSquare == 9)
                 {
                     x = 2;
                     y = 2;
@@ -440,63 +432,63 @@ void TTT_W::GameTTT()
             Sleep(500); 	// delay for 500 milliseconds
             //precondition: square is not filled
             //postcondition: square filled with "X"
-            if ((turnSquare == upperL) && (upperLeft == false)) 
+            if ((turnSquare == upperL) && (upperLeft == false))
             {
                 upperLeft = true;
                 ticBoard[0][0] = 1;
             }
             //precondition: square is not filled
             //postcondition: square filled with "X"
-            if ((turnSquare == upperC) && (upperCenter == false)) 
+            if ((turnSquare == upperC) && (upperCenter == false))
             {
                 upperCenter = true;
                 ticBoard[0][1] = 1;
             }
             //precondition: square is not filled
             //postcondition: square filled with "X"
-            if ((turnSquare == upperR) && (upperRight == false)) 
+            if ((turnSquare == upperR) && (upperRight == false))
             {
                 upperRight = true;
                 ticBoard[0][2] = 1;
             }
             //precondition: square is not filled
             //postcondition: square filled with "X"
-            if ((turnSquare == centerL) && (centerLeft == false)) 
+            if ((turnSquare == centerL) && (centerLeft == false))
             {
                 centerLeft = true;
                 ticBoard[1][0] = 1;
             }
             //precondition: square is not filled
             //postcondition: square filled with "X"
-            if ((turnSquare == centerC) && (centerCenter == false)) 
+            if ((turnSquare == centerC) && (centerCenter == false))
             {
                 centerCenter = true;
                 ticBoard[1][1] = 1;
             }
             //precondition: square is not filled
             //postcondition: square filled with "X"
-            if ((turnSquare == centerR) && (centerRight == false)) 
+            if ((turnSquare == centerR) && (centerRight == false))
             {
                 centerRight = true;
                 ticBoard[1][2] = 1;
             }
             //precondition: square is not filled
             //postcondition: square filled with "X"
-            if ((turnSquare == bottomL) && (bottomLeft == false)) 
+            if ((turnSquare == bottomL) && (bottomLeft == false))
             {
                 bottomLeft = true;
                 ticBoard[2][0] = 1;
             }
             //precondition: square is not filled
             //postcondition: square filled with "X"
-            if ((turnSquare == bottomC) && (bottomCenter == false)) 
+            if ((turnSquare == bottomC) && (bottomCenter == false))
             {
                 bottomCenter = true;
                 ticBoard[2][1] = 1;
             }
             //precondition: square is not filled
             //postcondition: square filled with "X"
-            if ((turnSquare == bottomR) && (bottomRight == false)) 
+            if ((turnSquare == bottomR) && (bottomRight == false))
             {
                 bottomRight = true;
                 ticBoard[2][2] = 1;
@@ -507,35 +499,31 @@ void TTT_W::GameTTT()
         }
         //precondition: playerNow = false
         //postcondition: player's turn
-        if ((turnCurrent == 1) && (playerNow == false)) 
+        if ((turnCurrent == 1) && (playerNow == false))
         {
             playerNow = true;
         }
 
         //Board Fill
-        //precondition: player or computer takes turn
-        //postcondition: state of game is updated
-        if (upperLeft == true) 
+        if (upperLeft == true)
         {
-            if (ticBoard[0][0] == 2) 
+            if (ticBoard[0][0] == 2)
             {
                 symbolInSpace = 'O';
                 cout << symbolInSpace << "|";
             }
-            if (ticBoard[0][0] == 1) 
+            if (ticBoard[0][0] == 1)
             {
                 symbolInSpace = 'X';
                 cout << symbolInSpace << "|";
             }
         }
-        else 
+        else
         {
             symbolInSpace = ' ';
             cout << symbolInSpace << "|";
         }
-        //precondition: player or computer takes turn
-        //postcondition: state of game is updated
-        if (upperCenter == true) 
+        if (upperCenter == true)
         {
             if (ticBoard[0][1] == 2) {
                 symbolInSpace = 'O';
@@ -546,149 +534,135 @@ void TTT_W::GameTTT()
                 cout << symbolInSpace << "|";
             }
         }
-        else 
+        else
         {
             symbolInSpace = ' ';
             cout << symbolInSpace << "|";
         }
-        //precondition: player or computer takes turn
-        //postcondition: state of game is updated
-        if (upperRight == true) 
+        if (upperRight == true)
         {
-            if (ticBoard[0][2] == 2) 
+            if (ticBoard[0][2] == 2)
             {
                 symbolInSpace = 'O';
                 cout << symbolInSpace << endl;
             }
-            if (ticBoard[0][2] == 1) 
+            if (ticBoard[0][2] == 1)
             {
                 symbolInSpace = 'X';
                 cout << symbolInSpace << endl;
             }
         }
-        else 
+        else
         {
             symbolInSpace = ' ';
             cout << symbolInSpace << endl;
         }
         cout << "------" << endl;
-        //precondition: player or computer takes turn
-        //postcondition: state of game is updated
-        if (centerLeft == true) 
+        if (centerLeft == true)
         {
-            if (ticBoard[1][0] == 2) 
+            if (ticBoard[1][0] == 2)
             {
                 symbolInSpace = 'O';
                 cout << symbolInSpace << "|";
             }
-            if (ticBoard[1][0] == 1) 
+            if (ticBoard[1][0] == 1)
             {
                 symbolInSpace = 'X';
                 cout << symbolInSpace << "|";
             }
         }
-        else 
+        else
         {
             symbolInSpace = ' ';
             cout << symbolInSpace << "|";
         }
-        //precondition: player or computer takes turn
-        //postcondition: state of game is updated
-        if (centerCenter == true) 
+        if (centerCenter == true)
         {
-            if (ticBoard[1][1] == 2) 
+            if (ticBoard[1][1] == 2)
             {
                 symbolInSpace = 'O';
                 cout << symbolInSpace << "|";
             }
-            if (ticBoard[1][1] == 1) 
+            if (ticBoard[1][1] == 1)
             {
                 symbolInSpace = 'X';
                 cout << symbolInSpace << "|";
             }
         }
-        else 
+        else
         {
             symbolInSpace = ' ';
             cout << symbolInSpace << "|";
         }
-        //precondition: player or computer takes turn
-        //postcondition: state of game is updated
-        if (centerRight == true) 
+        if (centerRight == true)
         {
-            if (ticBoard[1][2] == 2) 
+            if (ticBoard[1][2] == 2)
             {
                 symbolInSpace = 'O';
                 cout << symbolInSpace << endl;
             }
-            if (ticBoard[1][2] == 1) 
+            if (ticBoard[1][2] == 1)
             {
                 symbolInSpace = 'X';
                 cout << symbolInSpace << endl;
             }
         }
-        else 
+        else
         {
             symbolInSpace = ' ';
             cout << symbolInSpace << endl;
         }
         cout << "------" << endl;
-        //precondition: player or computer takes turn
-        //postcondition: state of game is updated
-        if (bottomLeft == true) 
+        if (bottomLeft == true)
         {
-            if (ticBoard[2][0] == 2) 
+            if (ticBoard[2][0] == 2)
             {
                 symbolInSpace = 'O';
                 cout << symbolInSpace << "|";
             }
-            if (ticBoard[2][0] == 1) 
+            if (ticBoard[2][0] == 1)
             {
                 symbolInSpace = 'X';
                 cout << symbolInSpace << "|";
             }
         }
-        else 
+        else
         {
             symbolInSpace = ' ';
             cout << symbolInSpace << "|";
         }
-        //precondition: player or computer takes turn
-        //postcondition: state of game is updated
-        if (bottomCenter == true) 
+        if (bottomCenter == true)
         {
-            if (ticBoard[2][1] == 2) 
+            if (ticBoard[2][1] == 2)
             {
                 symbolInSpace = 'O';
                 cout << symbolInSpace << "|";
             }
-            if (ticBoard[2][1] == 1) 
+            if (ticBoard[2][1] == 1)
             {
                 symbolInSpace = 'X';
                 cout << symbolInSpace << "|";
             }
         }
-        else 
+        else
         {
             symbolInSpace = ' ';
             cout << symbolInSpace << "|";
         }
-        //precondition: player or computer takes turn
-        //postcondition: state of game is updated
-        if (bottomRight == true) 
+        if (bottomRight == true)
         {
-            if (ticBoard[2][2] == 2) 
+            if (ticBoard[2][2] == 2)
             {
                 symbolInSpace = 'O';
                 cout << symbolInSpace << endl << endl;
             }
-            if (ticBoard[2][2] == 1) 
+            if (ticBoard[2][2] == 1)
             {
                 symbolInSpace = 'X';
                 cout << symbolInSpace << endl << endl;
             }
         }
-        else 
+        else
         {
             symbolInSpace = ' ';
             cout << symbolInSpace << endl << endl;
@@ -717,21 +691,21 @@ void TTT_W::GameTTT()
         }
         //precondition: a three in a row is detected made of "X"
         //postcondition: player wins
-        if ((ticBoard[0][0] == 1) && (ticBoard[1][0] == 1) && (ticBoard[2][0] == 1)) 
+        if ((ticBoard[0][0] == 1) && (ticBoard[1][0] == 1) && (ticBoard[2][0] == 1))
         {
             winCondition = true;
             cout << "Player Wins!" << endl;
         }
         //precondition: a three in a row is detected made of "X"
         //postcondition: player wins
-        if ((ticBoard[0][1] == 1) && (ticBoard[1][1] == 1) && (ticBoard[2][1] == 1)) 
+        if ((ticBoard[0][1] == 1) && (ticBoard[1][1] == 1) && (ticBoard[2][1] == 1))
         {
             winCondition = true;
             cout << "Player Wins!" << endl;
         }
         //precondition: a three in a row is detected made of "X"
         //postcondition: player wins
-        if ((ticBoard[0][2] == 1) && (ticBoard[1][2] == 1) && (ticBoard[2][2] == 1)) 
+        if ((ticBoard[0][2] == 1) && (ticBoard[1][2] == 1) && (ticBoard[2][2] == 1))
         {
             winCondition = true;
             cout << "Player Wins!" << endl;
@@ -745,7 +719,7 @@ void TTT_W::GameTTT()
         }
         //precondition: a three in a row is detected made of "X"
         //postcondition: player wins
-        if ((ticBoard[2][0] == 1) && (ticBoard[1][1] == 1) && (ticBoard[0][2] == 1)) 
+        if ((ticBoard[2][0] == 1) && (ticBoard[1][1] == 1) && (ticBoard[0][2] == 1))
         {
             winCondition = true;
             cout << "Player Wins!" << endl;
