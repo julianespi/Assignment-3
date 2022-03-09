b99d577bf880089a5e4f764b42f556e2b3229f61
Julian Espinoza <99230462+julianespi@users.noreply.github.com>
2022-03-08 22:23:10 -0800

    Update n-Queens.h

--- n-Queens.h
+++ n-Queens.h
@@ -1,13 +1,20 @@
 #pragma once
-void displayBoard(string**nQueens, int sizeOfBoard)
+const int QUEENSVLAUE = 100;
+
+void displayBoard(int**nQueens, int sizeOfBoard)
 {
     // displays the bored
     for (int i = 0; i < sizeOfBoard; i++) {
         for (int j = 0; j < sizeOfBoard; j++) {
             if (j == 0)
                 cout << "|";
-            cout << nQueens[i][j]
-                << "|";
+            if (nQueens[i][j] == 9)
+                cout << "Q";
+            else if (nQueens[i][j] > 0)
+                cout << "*";
+            else
+                cout << " ";
+                 cout << "|";
             if (j == sizeOfBoard - 1)
                 cout << endl;
         }
@@ -22,24 +29,64 @@ void displayBoard(string**nQueens, int sizeOfBoard)
     }
 }
 
-void addQueen(string **&nQueens, int sizeOfBoard)
+void displayBoardValues(int** nQueens, int sizeOfBoard)
+{
+    // displays the bored
+    for (int i = 0; i < sizeOfBoard; i++) {
+        for (int j = 0; j < sizeOfBoard; j++) {
+            if (j == 0)
+                cout << "|";
+
+                cout << nQueens[i][j] << " ";
+            cout << "|";
+            if (j == sizeOfBoard - 1)
+                cout << endl;
+        }
+        for (int i = 0; i < sizeOfBoard; i++)
+        {
+            if (i == 0)
+                cout << "-";
+            cout << "--";
+            if (i == sizeOfBoard - 1)
+                cout << endl;
+        }
+    }
+}
+
+void addQueen(int **&nQueens, int sizeOfBoard)
 {
     int row = inputInteger("enter the row you want to put a queen: ", 1, sizeOfBoard) - 1;
     int coloum = inputInteger("enter the coloum you want to put a queen: ", 1, sizeOfBoard) - 1;
+    
+    displayBoardValues(nQueens, sizeOfBoard);
+    cout << endl << endl;
 
-    nQueens[row][coloum] = "Q";
+        if (nQueens[row][coloum] == QUEENSVLAUE)
+        {
+            cout << "Invalid. A queen is already placed here. Try Again." << endl;
+            return;
+        }
+        else if (nQueens[row][coloum] > 0) 
+        {
+            cout << "Invalid. in the path of a current Queen. Try Again." << endl;
+            return;
+        }
+        else 
+            nQueens[row][coloum] = QUEENSVLAUE;
+
+    
 
     for (int i = 0; i < sizeOfBoard; i++)
     {
 
         if (coloum != i)
-            nQueens[row][i] = "*";
+            nQueens[row][i] += 1;
     }
 
     for (int i = 0; i < sizeOfBoard; i++)
     {
         if (row != i)
-            nQueens[i][coloum] = "*";
+            nQueens[i][coloum] += 1;
     }
 
     //handle diagonal line going bottom right.
@@ -56,48 +103,166 @@ void addQueen(string **&nQueens, int sizeOfBoard)
             if(i == row +temp1 && j == coloum + temp1)
             {
                 if (row != i && coloum != j)
-                    nQueens[i][j] = "*";
+                    nQueens[i][j] += 1;
+            }
+
+        }
+    }
+
+    temp1 = -1;
+    for (int i = row; i > -1; i--)
+    {
+        temp1++;
+        if (i <= -1)
+            break;
+        for (int j = coloum; j > -1; j--) {
+
+            if (j <= -1)
+                break;
+            if (i == row - temp1 && j == coloum - temp1)
+            {
+                if (row != i && coloum != j)
+                    nQueens[i][j] += 1;
+            }
+
+        }
+    }
+
+    temp1 = -1;
+    for (int i = row; i < sizeOfBoard; i++)
+    {
+        temp1++;
+        if (i >= sizeOfBoard)
+            break;
+        for (int j = coloum; j > -1; j--) {
+
+            if (j <= -1)
+                break;
+            if (i == row + temp1 && j == coloum - temp1)
+            {
+                if (row != i && coloum != j)
+                    nQueens[i][j] += 1;
+            }
+
+        }
+    }
+
+    temp1 = -1;
+    for (int i = row; i > -1; i--)
+    {
+        temp1++;
+        if (i <= -1)
+            break;
+        for (int j = coloum; j < sizeOfBoard; j++) {
+
+            if (j >= sizeOfBoard)
+                break;
+            if (i == row - temp1 && j == coloum + temp1)
+            {
+                if (row != i && coloum != j)
+                    nQueens[i][j] += 1;
             }
 
         }
     }
 
     displayBoard(nQueens, sizeOfBoard);
-    //not working 
-    //cout << endl;
-    //for (int i = row; i >= 0; i--) {
-    //    if (i <= 0)
-    //        break;
-    //    for (int j = coloum; j >= 0; j--) {
-    //        if (j <= 0)
-    //            break;
-    //        // Assign values to the
-    //        if (row != i && coloum != j )
-    //            nQueens[i][j] = "*";
-    //    }
-    //}
 
-
-    //displayBoard(nQueens, sizeOfBoard);
 }
 
-void removeQueen(string **&nQueens, int sizeOfBoard)
+void removeQueen(int **&nQueens, int sizeOfBoard)
 {
     int row = inputInteger("enter the row you want to put a queen: ", 1, sizeOfBoard) - 1;
     int coloum = inputInteger("enter the coloum you want to put a queen: ", 1, sizeOfBoard) - 1;
-    nQueens[row][coloum] = " ";
+    nQueens[row][coloum] = 0;
 
     for (int i = 0; i < sizeOfBoard; i++)
     {
 
         if (coloum != i)
-            nQueens[row][i] = " ";
+            nQueens[row][i] -= 1;
     }
 
     for (int i = 0; i < sizeOfBoard; i++)
     {
         if (row != i)
-            nQueens[i][coloum] = " ";
+            nQueens[i][coloum] -= 1;
+    }
+
+    int temp1 = -1;
+    for (int i = row; i < sizeOfBoard; i++)
+    {
+        temp1++;
+        if (i >= sizeOfBoard)
+            break;
+        for (int j = coloum; j < sizeOfBoard; j++) {
+
+            if (j >= sizeOfBoard)
+                break;
+            if (i == row + temp1 && j == coloum + temp1)
+            {
+                if (row != i && coloum != j)
+                    nQueens[i][j] -= 1;
+            }
+
+        }
+    }
+
+    temp1 = -1;
+    for (int i = row; i > -1; i--)
+    {
+        temp1++;
+        if (i <= -1)
+            break;
+        for (int j = coloum; j > -1; j--) {
+
+            if (j <= -1)
+                break;
+            if (i == row - temp1 && j == coloum - temp1)
+            {
+                if (row != i && coloum != j)
+                    nQueens[i][j] -= 1;
+            }
+
+        }
+    }
+
+    temp1 = -1;
+    for (int i = row; i < sizeOfBoard; i++)
+    {
+        temp1++;
+        if (i >= sizeOfBoard)
+            break;
+        for (int j = coloum; j > -1; j--) {
+
+            if (j <= -1)
+                break;
+            if (i == row + temp1 && j == coloum - temp1)
+            {
+                if (row != i && coloum != j)
+                    nQueens[i][j] -= 1;
+            }
+
+        }
+    }
+
+    temp1 = -1;
+    for (int i = row; i > -1; i--)
+    {
+        temp1++;
+        if (i <= -1)
+            break;
+        for (int j = coloum; j < sizeOfBoard; j++) {
+
+            if (j >= sizeOfBoard)
+                break;
+            if (i == row - temp1 && j == coloum + temp1)
+            {
+                if (row != i && coloum != j)
+                    nQueens[i][j] -= 1;
+            }
+
+        }
     }
 
     displayBoard(nQueens, sizeOfBoard);
@@ -120,14 +285,14 @@ void nQueensGame()
 {
     int sizeOfBoard = inputInteger("Enter the size of the bored you want to play on: ", 1, 10);
 
-    string** nQueens = new string * [sizeOfBoard];
+    int** nQueens = new int * [sizeOfBoard];
 
     //creates the array and fills the array with blank spaces
     for (int i = 0; i < sizeOfBoard; i++) {
 
         // Declare a memory block
         // of size sizeOfBoard
-        nQueens[i] = new string[sizeOfBoard];
+        nQueens[i] = new int[sizeOfBoard];
     }
 
     // Traverse the 2D array
@@ -136,7 +301,7 @@ void nQueensGame()
 
             // Assign values to the
             // memory blocks created
-            nQueens[i][j] = " ";
+            nQueens[i][j] = 0;
         }
     }
 
@@ -144,7 +309,7 @@ void nQueensGame()
     {
         switch (nQueenMenu())
         {
-        case 0: exit(1); break;
+        case 0: return; break;
         case 1: addQueen(nQueens, sizeOfBoard); break;
         case 2: removeQueen(nQueens, sizeOfBoard); break;
         default: cout << "\t\tERROR - Invalid option. Please re-enter."; break;
@@ -159,3 +324,4 @@ void nQueensGame()
         delete[] nQueens[i];
     delete[] nQueens;
 }
+;
